e0d16c73830fcf3003ed21d0c3e81ea9d2b0a4d0
hoang261105 <huyhoangcs2611@gmail.com>
2024-09-04 10:28:07 +0700

    Add

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,12 @@
-// import { authMiddleware } from '@clerk/nextjs';
-import { authMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-import type { NextFetchEvent, NextRequest } from "next/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default authMiddleware({
-  //@ts-ignore
-  afterAuth(auth, req: NextRequest, evt: NextFetchEvent) {
-    // handle users who aren't authenticated
-    if (!auth.userId && !auth.isPublicRoute) {
-      const signInUrl = new URL("/sign-in", req.url);
-      // signInUrl.searchParams.set('redirect_url', req.url);
-      return NextResponse.redirect(signInUrl);
-    }
-  },
-});
+export default clerkMiddleware();
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
 };
